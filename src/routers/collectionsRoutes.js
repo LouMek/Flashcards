c5b97b4e681c2806b66fca0fa2bd509200a481ba
Mona-94 <bapt50560@gmail.com>
2025-12-18 08:35:32 +0100

    feat: le delete fonctionne pour les collections

--- src/routers/collectionsRoutes.js
+++ src/routers/collectionsRoutes.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import { createCollection, deleteCollection, getAllCollections, getOneCollection } from '../controllers/collectionsController.js';
+import { authenticateToken } from '../middleware/authenticateToken.js';
 
 const router = Router();
+
+router.use(authenticateToken);
 
 router.get('/', getAllCollections);
 router.get('/:id', getOneCollection);
